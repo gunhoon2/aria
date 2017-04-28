51c08ccd8f12ba2a05a84958f8e3759d334ecceb
Gunhoon Lee <gunhoon@gmail.com>
2017-04-28 23:14:15 +0900

    Set up a minimal cmake build

---
+++ examples/simple_example.cc
@@ -0,0 +1,10 @@
+// Copyright (c) 2017, Gunhoon Lee (gunhoon@gmail.com)
+
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+  if (argc > 0)
+    std::cout << argv[0] << " is started..\n";
+
+  return 0;
+}
