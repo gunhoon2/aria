823e55b93c5b0c4c811129c34097a1bcee973363
Gunhoon Lee <gunhoon@gmail.com>
2017-06-22 12:07:07 +0900

    Modify the logging macro (#4)
    Also, add the LOG_IF, CHECK and LOG_ASSERT macro.

--- aria/base/logging.h
+++ aria/base/logging.h
@@ -36,19 +36,51 @@ class LogMessage {
   LogMessage& operator=(const LogMessage&) = delete;
 };
 
+// This class is used to explicitly ignore values in the conditional
+// logging macros. This avoids compiler warnings like "value computed
+// is not used" and "statement has no effect".
+class LogMessageVoidify {
+ public:
+  LogMessageVoidify() { }
+  // This has to be an operator with a precedence lower than << but
+  // higher than ?:
+  void operator&(std::ostream&) { }
+};
+
 
 // ---------------------- Logging Macro definitions --------------------------
 
 #define _ARIA_LOG_INFO \
-  aria::logging::LogMessage(__FILE__, __LINE__, aria::logging::LOG_INFO).stream()
+  aria::logging::LogMessage(__FILE__, __LINE__, aria::logging::LOG_INFO)
 #define _ARIA_LOG_WARNING \
-  aria::logging::LogMessage(__FILE__, __LINE__, aria::logging::LOG_WARNING).stream()
+  aria::logging::LogMessage(__FILE__, __LINE__, aria::logging::LOG_WARNING)
 #define _ARIA_LOG_ERROR \
-  aria::logging::LogMessage(__FILE__, __LINE__, aria::logging::LOG_ERROR).stream()
+  aria::logging::LogMessage(__FILE__, __LINE__, aria::logging::LOG_ERROR)
 #define _ARIA_LOG_FATAL \
-  aria::logging::LogMessage(__FILE__, __LINE__, aria::logging::LOG_FATAL).stream()
+  aria::logging::LogMessage(__FILE__, __LINE__, aria::logging::LOG_FATAL)
 
-#define LOG(severity) _ARIA_LOG_##severity
+// TODO: should implement
+#define LOG_IS_ON(severity) (true)
+
+// Macro which call the stream() member function of LogMessage
+#define LOG_STREAM(severity) _ARIA_LOG_##severity.stream()
+
+// Helper macro which avoids evaluating the arguments to a stream if
+// the condition doesn't hold. Condition is evaluated once and only once.
+#define LAZY_STREAM(stream, condition) \
+  !(condition) ? (void) 0 : aria::logging::LogMessageVoidify() & (stream)
+
+#define LOG(severity) LAZY_STREAM(LOG_STREAM(severity), LOG_IS_ON(severity))
+#define LOG_IF(severity, condition) \
+  LAZY_STREAM(LOG_STREAM(severity), LOG_IS_ON(severity) && (condition))
+
+#define CHECK(condition) \
+  LOG_IF(FATAL, !(condition)) << "Check failed: " #condition ". "
+#define DCHECK(condition) \
+  LOG_IF(FATAL, !(condition)) << "Check failed: " #condition ". "
+
+#define LOG_ASSERT(condition) \
+  LOG_IF(FATAL, !(condition)) << "Assert failed: " #condition ". "
 
 }  // namespace logging
 }  // namespace aria
--- aria/base/logging_test.cc
+++ aria/base/logging_test.cc
@@ -10,6 +10,17 @@ TEST(LoggingTest, Logging) {
   LOG(INFO) << "Hello World!";
   LOG(WARNING) << "Warning message";
   LOG(ERROR) << "Error message";
+
+  LOG_IF(INFO, true) << "LOG_IF message when condition is true.";
+  LOG_IF(INFO, false) << "LOG_IF message when condition is false.";
+
+  CHECK(true);
+  //CHECK(false);
+  DCHECK(true);
+  //DCHECK(false);
+
+  LOG_ASSERT(true);
+  //LOG_ASSERT(false);
 }
 
 }  // namespace aria
