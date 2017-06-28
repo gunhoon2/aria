59da13ec6a540e988e6228deaed25fb9c4e52d76
Gunhoon Lee <gunhoon@gmail.com>
2017-06-28 22:27:12 +0900

    Implement AutoLock class wrapping a mutex (#13)

---
+++ aria/base/thread/lock.h
@@ -0,0 +1,32 @@
+// Copyright (c) 2017, Gunhoon Lee (gunhoon@gmail.com)
+
+#ifndef ARIA_BASE_THREAD_LOCK_H_
+#define ARIA_BASE_THREAD_LOCK_H_
+
+#include "aria/base/thread/mutex.h"
+
+namespace aria {
+
+// The class AutoLock is a mutex wrapper that provides a convenient RAII-style
+// mechanism for owning a mutex for the duration of a scoped block.
+// This is similar to std::lock_guard class.
+class AutoLock {
+ public:
+  explicit AutoLock(Mutex& m) : mutex_(m) {
+    mutex_.Lock();
+  }
+
+  ~AutoLock() {
+    mutex_.Unlock();
+  }
+
+ private:
+  Mutex& mutex_;
+
+  AutoLock(const AutoLock&) = delete;
+  AutoLock& operator=(const AutoLock&) = delete;
+};
+
+}  // namespace aria
+
+#endif  // ARIA_BASE_THREAD_LOCK_H_
